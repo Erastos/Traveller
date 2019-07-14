f4514703786d4cf53d4e47bb8954ec1c5286608e
Andrew Smith <erastos.gentry@gmail.com>
2019-07-14 02:21:25 -0500

    Added Basic Trip Interface

--- cli/cli.py
+++ cli/cli.py
@@ -1,10 +1,11 @@
-#!/bin/python3.7
+#!/home/erastos/projects/travel/cli/venv/bin/python
 import sys
 import requests
 import json
 from amadeus import Client, ResponseError
 import argparse
 import datetime
+import csv
 
 # Parse Args
 parser = argparse.ArgumentParser(prog="Traveller", description="Flight Planner Application")
@@ -14,11 +15,20 @@ parser.add_argument("dest_date", action="store", help="Date of Arrival at Destin
 
 args = parser.parse_args()
 
+# Get Airport Codes
+airport_csv = open("airports.csv")
+airport_csv = csv.reader(airport_csv)
+airport_processed = {}
+for row in airport_csv:
+    if not row[13]:
+        airport_processed[row[1]] = row[3]
+    else:
+        airport_processed[row[13]] = row[3]
 # Amadeus Login
 API_KEY = open("key.env").read().strip()
-print("Key", API_KEY)
+print(API_KEY)
 API_SECRET = open("secret.env").read().strip()
-print("Secret", API_SECRET)
+print(API_SECRET)
 
 # Get Lat and Long
 unformated_body = requests.get("http://ip-api.com/json").content.decode("utf-8")
@@ -26,13 +36,13 @@ body = json.loads(unformated_body)
 lat = body["lat"]
 long = body["lon"]
 
-# Get IATA Codes from Airport
-amadeus = Client(
-    client_id=API_KEY,
-    client_secret=API_SECRET
-)
 try:
-    # Get Closest Aiports
+    # Get IATA Codes from Airport
+    amadeus = Client(
+        client_id=API_KEY,
+        client_secret=API_SECRET
+    )
+    # Get Closest Airports
     response = amadeus.reference_data.locations.airports.get(
         latitude=lat,
         longitude=long
@@ -40,13 +50,24 @@ try:
     iata = [element["iataCode"] for element in response.data]
 
     # Get Flights from those airports
-    for code in iata[:2]:
+    t_n = 1
+    for code in iata[:1]:
         flights = amadeus.shopping.flight_offers.get(
             origin=code,
             destination=args.dest,
-            departureDate=args.dest_date
+            departureDate=args.dest_date,
+            max=3
         )
-        print(flights.data)
+        body = json.loads(flights.body)
+        for trip in flights.data:
+            output = "{0}:".format(t_n)
+            for flight in trip["offerItems"][0]["services"][0]["segments"]:
+                iata_code = flight["flightSegment"]["departure"]["iataCode"]
+                output += " {0} ({1}) --->".format(iata_code, body["dictionaries"]["locations"][iata_code]["detailedName"])
+            arrival = flight["flightSegment"]["arrival"]["iataCode"]
+            output += " {0} ({1})".format(arrival, body["dictionaries"]["locations"][arrival]["detailedName"])
+            print(output)
+            t_n += 1
 except ResponseError as e:
     print(e)
     sys.exit(1)
